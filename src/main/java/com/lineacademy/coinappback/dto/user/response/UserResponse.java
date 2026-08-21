1b29125bc4471665e22deb769c89ed7cfcc7c248
ehdgus02 <ehdgus02kk@naver.com>
2026-08-21 11:52:16 +0900

    User 기능 작성

---
+++ src/main/java/com/lineacademy/coinappback/dto/user/response/UserResponse.java
@@ -0,0 +1,22 @@
+package com.lineacademy.coinappback.dto.user.response;
+
+import com.lineacademy.coinappback.domain.entity.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserResponse {
+    private Long id;
+    private String email;
+    private String nickname;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .build();
+    }
+
+}
