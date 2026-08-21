fe0930759507d9ba76f05d949c220207c90b9e72
Tae.Garam (Office) <drasla217@gmail.com>
2026-08-21 09:53:01 +0900

    초기화

---
+++ src/test/java/com/lineacademy/coinappback/CoinAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lineacademy.coinappback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinAppBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
