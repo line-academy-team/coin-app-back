70c2227dbe7409477726f3b9a94f44a71aaec918
molang404 <art104233@naver.com>
2026-08-21 11:10:52 +0900

    수정

--- src/main/java/com/lineacademy/coinappback/domain/entity/Portfolio.java
+++ src/main/java/com/lineacademy/coinappback/domain/entity/Portfolio.java
@@ -7,9 +7,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import com.lineacademy.coinappback.domain.entity.User;
 import java.math.BigDecimal;
-import com.lineacademy.coinappback.domain.entity.PortfolioItem;
 import java.util.ArrayList;
 import java.util.List;
 
