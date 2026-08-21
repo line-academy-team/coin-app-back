5cc273f0720b1305e7f2656f77e9536334fdb0a9
mcm <joohyuna2@gmail.com>
2026-08-21 10:44:54 +0900

    BastTimeEntity

---
+++ src/main/java/com/lineacademy/coinappback/domain/common/BaseTimeEntity.java
@@ -0,0 +1,32 @@
+package com.lineacademy.coinappback.domain.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false, name = "created_at")
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    public void markAsDeleted() {
+        this.deletedAt = LocalDateTime.now();
+    }
+
+}
